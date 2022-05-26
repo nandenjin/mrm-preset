535c9e8f1dcb7f9060c8dc53f4dca3e8522bbf2f
Kazumi Inada <hello@nandenjin.com>
2022-05-26 14:45:24 +0900

    fix: Extract gitignore response as plain text

--- gitignore/index.js
+++ gitignore/index.js
@@ -2,6 +2,6 @@ const { lines } = require('mrm-core')
 const fetch = require('sync-fetch')
 
 module.exports = () => {
-  const content = fetch('https://www.toptal.com/developers/gitignore/api/node')
+  const content = fetch('https://www.toptal.com/developers/gitignore/api/node').text()
   lines('.gitignore').add(content).save()
 }
